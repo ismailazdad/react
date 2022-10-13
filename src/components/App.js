dc560d12824dadc7eefedf30764ce2ad69d8d809
Ismail Azdad <ismail.azdad@adventori.com>
2022-10-13 17:19:10 +0200

    add Shoppinglist.js and plantList.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import '../style/App.css';
 import Banner from './Banner'
 import Cart from './Cart'
+import ShoppingList from "./Shoppinglist";
 
 function App() {
     return (
         <div>
             <Banner/>
             <Cart/>
+            <ShoppingList />
         </div>
     );
 }
